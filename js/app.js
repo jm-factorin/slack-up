f093cdb93b7bdcc7066e8c6c5501715d269c3c23
jm-factorin <jm.factorin@klab.com>
2016-04-08 16:19:29 +0800

    v1.6.0 -- added import/export json files -- fixed modals (use generic_dialog)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function () {
     var code = function () {
         var HIDDEN_MESSAGE_KEY = 'conf_hide_msg_ids';
+        var CONF_PREFIX = 'conf_sidebar_';
 
         function _sort(a, b) {
             function getChannelName(c) {
@@ -33,6 +34,11 @@
             TS.client.channel_pane.rebuildChannelList();
         }
 
+        function _getConfData(id) {
+            var json = window.localStorage.getItem(CONF_PREFIX + id);
+            return json ? JSON.parse(json) : {"icon": "", "alias": ""};
+        }
+
         function _injectChangeInfo(html) {
             var $div = $('<div>').html(html);
             var $li = $('<li>', {id: "channel_info_change"}).html('<a>Set Channel Info</a>');
@@ -48,7 +54,7 @@
             var icon = (type == 'group') ? 'ts_icon_lock' : 'ts_icon_channel_pane_hash';
 
             try {
-                var data = ChangeIconDialog.getData($id);
+                var data = _getConfData($id);
                 if (!data.icon && !data.alias) {
                     return html;
                 }
@@ -81,61 +87,132 @@
             }
         }
 
-        var ChangeIconDialog = {
-            getData: function (id) {
-                var json = window.localStorage.getItem(ChangeIconDialog.getChannelKey(id));
-                return json ? JSON.parse(json) : {"icon": "", "alias": ""};
-            },
-
-            getChannelKey: function (id) {
-                return 'conf_sidebar_' + id;
-            },
-
-            html: function () {
-                return '<div class="modal-header"><button type="button" class="close" data-dismiss="modal" '
-                    + 'aria-hidden="true">×</button><h3>Set info for channel #<span id="ch_title"></span></h3></div>'
-                    + '<div class="modal-body"><p class="top_margin">'
-                    + '<label for="ch_alias" class="inline_block">Sidebar Alias</label>'
-                    + '<input id="ch_alias" type="text" class="small">'
-                    + '<span class="modal_input_note">Add channel alias. '
-                    + 'Set a custom sidebar name for this channel.</span>'
-                    + '</p><p class="top_margin">'
-                    + '<label for="ch_icon" class="inline_block">Emoji Icon</label>'
-                    + '<input id="ch_icon" type="text" class="small">'
-                    + '<span class="modal_input_note">Change channel emoji icon. '
-                    + 'Paste your emoji code here like <code>:book:</code></span>'
-                    + '</p></div>'
-                    + '<div class="modal-footer"><a class="btn btn_save">Save Settings</a></div>';
-            },
-
-            show: function (title) {
-                var $div = $('div#change_icon_dialog');
-                if (!$div.length) {
-                    $div = $('<div>', {
-                        "id": "change_icon_dialog",
-                        "class": "modal hide fade in",
-                        "aria-hidden": "false"
-                    }).html(ChangeIconDialog.html());
-                    $(document.body).append($div);
+        function _getChannelConfSettings() {
+            var json = {};
+            var keys = Object.keys(window.localStorage);
+            for (var i = 0; i < keys.length; i++) {
+                var k = keys[i];
+                if (k.indexOf(CONF_PREFIX) == 0) {
+                    json[k] = JSON.parse(window.localStorage[k]);
                 }
+            }
 
-                var data = ChangeIconDialog.getData(TS.model.active_cid);
-                $div.find('#ch_icon').val(data.icon);
-                $div.find('#ch_alias').val($('<span>').html(data.alias).text());
-                $div.find('span#ch_title').text(title);
-                $div.find('a.btn_save').click(function (e) {
-                    e.preventDefault();
-                    window.localStorage.setItem(ChangeIconDialog.getChannelKey(TS.model.active_cid), JSON.stringify({
-                        'icon': TS.utility.htmlEntities($('#ch_icon').val().trim()),
-                        'alias': TS.utility.htmlEntities($('#ch_alias').val().trim())
+            return json;
+        }
+
+        function _downloadConfigFile() {
+            var data = JSON.stringify(_getChannelConfSettings());
+            var blob = new Blob([data]);
+            var e = document.createEvent("HTMLEvents");
+            e.initEvent("click");
+            $("<a>", {
+                download: 'slack-up.' + TS.model.team.domain + '.config.json',
+                href: URL.createObjectURL(blob)
+            }).get(0).dispatchEvent(e);
+        }
+
+        function _uploadConfigFile(e) {
+            try {
+                var file = e.target.files[0];
+                if (file != null) {
+                    var reader = new FileReader();
+                    reader.addEventListener('load', function(evt) {
+                        var conf = JSON.parse(evt.target.result);
+                        var keys = Object.keys(conf);
+                        for (var i = 0; i < keys.length; i++) {
+                            var key = keys[i];
+                            if (key.indexOf(CONF_PREFIX) == 0) {
+                                window.localStorage.setItem(key, JSON.stringify({
+                                    'icon': conf[key].icon,
+                                    'alias': conf[key].alias
+                                }));
+                            }
+                        }
+
+                        _rebuildClientPage();
+                        TS.ui.inline_saver.show({target: TS.generic_dialog.div.find('a#upload_conf_btn')});
+                    });
+                    reader.readAsText(file);
+                }
+            } catch (ex) {
+                console.error(ex);
+            }
+        }
+
+        function _buildImportExportDialog() {
+            return '<p class="bold no_bottom_margin">'
+                + '<a id="download_conf_btn" class="bold">Export channel configuration file</a></p>'
+                + '<p style="line-height:1.25rem;">Download all your channel settings to a json file. '
+                + 'You can share this to others to sync their settings with yours.</p>'
+                + '<p class="bold no_bottom_margin"><a id="upload_conf_btn" class="bold">'
+                + 'Import channel setting json file</a></p>'
+                + '<input type="file" style="display:none" id="import_json" accept=".json" />'
+                + '<p style="line-height: 1.25rem;margin-bottom:0">'
+                + 'Upload <code>json</code> file and automatically update your channel aliases and icons</p>';
+        }
+
+        function _buildChangeChannelInfoDialog() {
+            return '<p class="top_margin">'
+                + '<label for="ch_alias" class="inline_block">Sidebar Alias</label>'
+                + '<input id="ch_alias" type="text" class="small">'
+                + '<span class="modal_input_note">Add channel alias. '
+                + 'Set a custom sidebar name for this channel.</span>'
+                + '</p><p class="top_margin">'
+                + '<label for="ch_icon" class="inline_block">Emoji Icon</label>'
+                + '<input id="ch_icon" type="text" class="small">'
+                + '<span class="modal_input_note" id="ch_info_save_target">Change channel emoji icon. '
+                + 'Paste your emoji code here like <code>:book:</code></span></p>';
+        }
+
+        function _showImportExportModalDialog() {
+            TS.generic_dialog.start({
+                title: "SlackUp Preferences",
+                body: _buildImportExportDialog(),
+                show_cancel_button: true,
+                show_go_button: false,
+                esc_for_ok: false,
+                onShow: function () {
+                    TS.generic_dialog.div
+                        .find(".modal-footer")
+                        .addClass("hidden");
+                }
+            });
+
+            TS.generic_dialog.div
+                .find('.modal-body input#import_json')
+                .change(_uploadConfigFile);
+        }
+
+        function _showChangeChannelInfoDialog() {
+            TS.generic_dialog.start({
+                title: "Set info for channel #" + TS.shared.getActiveModelOb().name,
+                body: _buildChangeChannelInfoDialog(),
+                show_cancel_button: true,
+                show_go_button: true,
+                go_button_text: "Save Settings",
+                esc_for_ok: false,
+                onShow: function () {
+                    var $div = TS.generic_dialog.div;
+                    var data = _getConfData(TS.model.active_cid);
+
+                    $div.find('#ch_alias').val($('<span>').html(data.alias).text());
+                    $div.find('#ch_icon').val(data.icon);
+                },
+                onGo: function() {
+                    var $div = TS.generic_dialog.div;
+                    var $icon = $div.find('#ch_icon').val().trim();
+                    var $alias = $div.find('#ch_alias').val().trim();
+
+                    window.localStorage.setItem(CONF_PREFIX + TS.model.active_cid, JSON.stringify({
+                        'icon': TS.utility.htmlEntities($icon),
+                        'alias': TS.utility.htmlEntities($alias)
                     }));
 
                     _rebuildClientPage();
-                    $div.modal('hide');
-                });
-                $div.modal('show');
-            }
-        };
+                }
+            });
+
+        }
 
         var _rebuildStarredList = TS.client.channel_pane.rebuildStarredList;
         TS.client.channel_pane.rebuildStarredList = function () {
@@ -180,7 +257,7 @@
         TS.templates.menu_message_action_items = function (a) {
             var $div = $('<div>').html(_menuMessageActionItems(a));
             var $li = $('<li>', {
-                "id" : "hide_link",
+                "id": "hide_link",
                 "class": "danger",
                 "data-ts-message-id": TS.templates.makeMsgDomId(a.msg.ts)
             }).html('<a>Hide message (for me)</a>');
@@ -245,11 +322,11 @@
         TS.templates.channel_header = function (a) {
             var $div = $('<div>').html(_channelHeaderTemplates(a));
             if (boot_data.hasOwnProperty('other_accounts') && Object.keys(boot_data.other_accounts).length) {
-                var $container = $('<div>', {"class": "team_list_info"});
+                var $container = $('<div>', {"class": "team_list_info", "style": "background:#FFF"});
                 var keys = Object.keys(boot_data.other_accounts);
-                keys.forEach(function(k) {
+                keys.forEach(function (k) {
                     var html = '<a class="blue_hover channel_actions_toggle channel_header_icon ts_tip ts_tip_bottom"'
-                        + ' href="' + boot_data.other_accounts[k]['team_url'] + '">'
+                        + ' href="' + boot_data.other_accounts[k]['team_url'] + '" target="_blank">'
                         + '<span class="ts_tip_tip">' + boot_data.other_accounts[k]['team_name'] + '</span>'
                         + '<img style="border-radius:3px;width:24px;height:24px;margin:4px 3px 0 0"'
                         + ' src="' + boot_data.other_accounts[k]["team_icon"]["image_34"] + '"></a>';
@@ -263,10 +340,22 @@
             return $div.html();
         };
 
+        var _menuStartWithTeamAndUser = TS.menu.startWithTeamAndUser;
+        TS.menu.startWithTeamAndUser = function (e) {
+            _menuStartWithTeamAndUser(e);
+            var $ul = $('section.slack_menu_you ul.menu_list');
+            var $li = $('<li>', {"id": "import_export_dialog", "role": "menuitem"});
+            var $a = $('<a>');
+
+            $a.html('SlackUp Preferences');
+            $li.append($a);
+            $ul.append($li);
+        };
+
         $(document.body).on('click', '[id="channel_info_change"]', function (e) {
             e.preventDefault();
-            ChangeIconDialog.show(TS.shared.getActiveModelOb().name);
             TS.menu.end();
+            _showChangeChannelInfoDialog();
         });
 
         $(document.body).on('click', '[id="hide_link"]', function (e) {
@@ -277,6 +366,23 @@
 
             window.localStorage.setItem(HIDDEN_MESSAGE_KEY, JSON.stringify(data));
             TS.client.msg_pane.rebuildMsgs();
+        });
+
+        $(document.body).on('click', '[id="import_export_dialog"]', function (e) {
+            e.preventDefault();
+            clearTimeout(TS.menu.end_time);
+            TS.menu.end();
+            _showImportExportModalDialog();
+        });
+
+        $(document.body).on('click', '[id="download_conf_btn"]', function (e) {
+            e.preventDefault();
+            _downloadConfigFile();
+        });
+
+        $(document.body).on('click', '[id="upload_conf_btn"]', function (e) {
+            e.preventDefault();
+            $('input#import_json').trigger('click');
         });
 
         _rebuildClientPage();
